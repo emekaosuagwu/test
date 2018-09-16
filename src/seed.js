4041addb9a295d9ad9a4d33ca00a1cf29cb0eee5
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2018-09-16 14:37:49 +0100

    add country seeder

--- src/seed.js
+++ src/seed.js
@@ -1,10 +1,10 @@
 import faker from "faker";
 import mongoose from 'mongoose';
 import UserModel from './models/user';
+import CountryModel from './models/country';
 
 var user_size = 10
 var nationality_size = 1
-
 
 /**
  * [User Model Seeder]
@@ -12,35 +12,20 @@ var nationality_size = 1
 for (var i = 1; i <= nationality_size; i++) {
 
 	var data = {
- 		first_name: faker.name.firstName(),
- 		last_name: faker.name.lastName(),
- 		middle_name: faker.name.firstName(),
- 		image: faker.image.avatar(),
- 		gender: "male",
- 		nationality: 1987878788787,
- 		languages: 1987878788787,
- 		email: faker.internet.email(),
- 		phone: faker.internet.ip(),
- 		password: faker.internet.password(),
- 		oauth_id: faker.internet.password(),
- 		account_type: faker.name.title(),
- 		reset_token: faker.name.title(),
- 		reset_token_expiry: faker.name.title(),
- 		access: {
- 			title: "fvdfvdfd",
- 			level: "vsdvsdcsdcsdcsd"
- 		}
+ 		country_name: faker.address.country(),
+ 		country_code: faker.address.countryCode(),
  	}
 
- 	console.log(data)
+	const Country = new CountryModel(data)
 
-	// const User = new UserModel(data)
+	Country.save((err, country_info) => {
+		
+		if (err) {
+			console.log(err)
+		}
 
-	// User.save((err, userInfo) => {
-	// 	if (err) {
-	// 		console.log(err)
-	// 	}
-	// })
+		console.log(country_info)
+	})
 }
 
 
