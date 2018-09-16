9f038b5417d7a0c421c4fc5f29072710fba92157
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2018-09-16 14:25:44 +0100

    update CountrySchema

--- src/seed.js
+++ src/seed.js
@@ -3,11 +3,13 @@ import mongoose from 'mongoose';
 import UserModel from './models/user';
 
 var user_size = 10
+var nationality_size = 1
+
 
 /**
  * [User Model Seeder]
  */
-for (var i = 1; i <= user_size; i++) {
+for (var i = 1; i <= nationality_size; i++) {
 
 	var data = {
  		first_name: faker.name.firstName(),
@@ -40,3 +42,41 @@ for (var i = 1; i <= user_size; i++) {
 	// 	}
 	// })
 }
+
+
+// /**
+//  * [User Model Seeder]
+//  */
+// for (var i = 1; i <= user_size; i++) {
+
+// 	var data = {
+//  		first_name: faker.name.firstName(),
+//  		last_name: faker.name.lastName(),
+//  		middle_name: faker.name.firstName(),
+//  		image: faker.image.avatar(),
+//  		gender: "male",
+//  		nationality: 1987878788787,
+//  		languages: 1987878788787,
+//  		email: faker.internet.email(),
+//  		phone: faker.internet.ip(),
+//  		password: faker.internet.password(),
+//  		oauth_id: faker.internet.password(),
+//  		account_type: faker.name.title(),
+//  		reset_token: faker.name.title(),
+//  		reset_token_expiry: faker.name.title(),
+//  		access: {
+//  			title: "fvdfvdfd",
+//  			level: "vsdvsdcsdcsdcsd"
+//  		}
+//  	}
+
+//  	console.log(data)
+
+// 	// const User = new UserModel(data)
+
+// 	// User.save((err, userInfo) => {
+// 	// 	if (err) {
+// 	// 		console.log(err)
+// 	// 	}
+// 	// })
+// }
