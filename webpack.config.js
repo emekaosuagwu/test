d94c2e0f593d722b2dd84b4c2aed8a0f3704975c
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2018-09-23 14:22:08 +0100

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,19 @@
-const path = require('path');
+ var path = require('path');
+ var webpack = require('webpack');
 
-module.exports = {
-	entry: './src/server.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js'
-	},
-	module: {
-	  rules: [
-	    {
-	      test: /\.js$/,
-	      exclude: /(node_modules)/,
-	      use: {
-	        loader: 'babel-loader',
-	      }
-	    }
-	  ]
-	}
-};
-
-
-
+ module.exports = {
+     entry: './src/server.js',
+     output: {
+         path: path.resolve(__dirname, 'build'),
+         filename: 'main.bundle.js'
+     },
+     module: {
+       rules: [
+         { 
+         	test: /\.js$/, 
+         	exclude: /node_modules/, 
+         	loader: "babel-loader" 
+         }
+       ]
+     }
+ };
